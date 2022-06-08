e206fd66f2a1ca4d078b65db6cbfa788c6f53c5f
Mélanie <melanief0968@gmail.com>
2022-06-08 13:46:59 +0200

    update env

--- firebase.config.js
+++ firebase.config.js
@@ -1,13 +1,13 @@
 const env = import.meta.env;
 
 const firebaseConfig = {
-  apiKey: env.VITE_apiKey,
-  authDomain: env.VITE_authDomain,
-  databaseURL: env.VITE_databaseURL,
-  projectId: env.VITE_projectId,
-  storageBucket: env.VITE_storageBucket,
-  messagingSenderId: env.VITE_messagingSenderId,
-  appId: env.VITE_appId,
+  apiKey: env.VITE_APIKEY,
+  authDomain: env.VITE_AUTHDOMAIN,
+  databaseURL: env.VITE_DATABASEURL,
+  projectId: env.VITE_PROJECTID,
+  storageBucket: env.VITE_STORAGEBUCKET,
+  messagingSenderId: env.VITE_MESSAGINGSENDERID,
+  appId: env.VITE_APPID,
 };
 
 export default firebaseConfig;
